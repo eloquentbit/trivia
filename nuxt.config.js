9a72fa26dff9480a7bd571842ea28355c8dfa997
Luca Peduto <luca@eloquentbit.com>
2017-01-01 17:46:52 +0100

    Add title properties on nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   loading: false,
   head: {
+    title: 'Trivia',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }

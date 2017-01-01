41360d0b4828fbb3b994fff1a2483f62d0f906f1
Luca Peduto <luca@eloquentbit.com>
2017-01-01 16:40:37 +0100

    Add custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 clas="title">
+      {{ error.statusCode }}
+    </h1>
+    <h2 class="info">
+      {{ error.message }}
+    </h2>
+    <nuxt-link to="/" v-id="error.statusCode === 404">
+      Homepage
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
+
+<style scoped>
+.title {
+  font-size: 5em;
+}
+.info {
+  font-weight: 300;
+  color: #9AABB1;
+}
+</style>

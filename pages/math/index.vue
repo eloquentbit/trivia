0becab928b94efbacd97d7545eca944d764e02b6
Luca Peduto <luca@eloquentbit.com>
2017-01-01 15:19:33 +0100

    Add math route

---
+++ pages/math/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <message :message="message"></message>
+    <links/>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import Message from '~components/Message.vue'
+  import Links from '~components/Links.vue'
+
+  export default {
+    components: {
+      Message,
+      Links,
+    },
+    data ({ params }) {
+      return axios.get('http://numbersapi.com/random/math?json')
+        .then((res) => {
+          return { message: res.data.text }
+        })
+    }
+  }
+</script>

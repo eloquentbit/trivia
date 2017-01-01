d7de1fa6ec8582d5443657bba01fe50405995e54
Luca Peduto <luca@eloquentbit.com>
2017-01-01 15:04:14 +0100

    Add Links component

---
+++ components/Links.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="links">
+      <ul>
+        <li>
+          <nuxt-link to="/">Trivia</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/math">Math</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/date">Date</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/year">Year</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>

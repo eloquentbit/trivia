42d2ca70eb9ddb42ada13f79f4d9e189a5570b2e
Luca Peduto <luca@eloquentbit.com>
2017-01-01 12:06:51 +0100

    Add custom font

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  head: {
+    link: [
+      { rel: 'stylesheet',
+        src: 'https://fonts.googleapis.com/css?family=Noto+Serif' }
+    ]
+  },
   build: {
     vendor: ['axios']
   },

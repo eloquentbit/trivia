27a9d5b570e601a103ccc406bcd000de0256ccda
Luca Peduto <luca@eloquentbit.com>
2017-01-01 15:05:30 +0100

    Add Links component to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="content">
+  <div>
     <div class="trivia">
       <p>{{ message }}</p>
     </div>
+    <links/>
   </div>
 </template>
 
 <script>
   import axios from 'axios'
+  import Links from '~components/Links.vue'
 
   export default {
+    components: {
+      Links
+    },
     data ({ params }) {
       return axios.get('http://numbersapi.com/random?json')
         .then((res) => {

f0fb41095b0bf3a414892223fe3dfe5e97099652
Luca Peduto <luca@eloquentbit.com>
2017-01-01 13:28:24 +0100

    Add meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
     link: [
       { rel: 'stylesheet',
         src: 'https://fonts.googleapis.com/css?family=Noto+Serif' }

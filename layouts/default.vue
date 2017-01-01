da012d363888a1489c16b0c24c7ec27d44b1210e
Luca Peduto <luca@eloquentbit.com>
2017-01-01 15:04:47 +0100

    Add Footer component and custom layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    <nuxt/>
+    <app-footer/>
+  </div>
+</template>
+
+<script>
+  import AppFooter from '~components/Footer.vue'
+
+  export default {
+    components: {
+      AppFooter
+    }
+  }
+</script>
